df57e7dacf7861ff2967e5d9b82d44ac913a5723
keyshasalsabila <salsaazhr8@gmail.com>
2025-12-09 08:50:28 +0700

    Create chatbot.py

---
+++ src/chatbot.py
@@ -0,0 +1,23 @@
+# Data pertanyaan dan jawaban (kamus)
+data = {
+    "kapan jadwal kuliah": "Jadwal kuliah dapat dilihat di portal akademik.",
+    "Bagaimana cara bayar ukt": "Pembayaran UKT melalui bank yang ditentukan kampus.",
+    "Kapan pengisian KRS dilakukan": "Dilakukan setiap awal semester sesuai kalender akademik.",
+    "Apakah kampus menyediakan beasiswa": "Ya, kampus menyediakan berbagai jenis beasiswa berdasarkan prestasi dan ekonomi.",
+    "Dimana saya bisa lihat nilai akhir": "Nilai dapat dilihat melalui portal akademik mahasiswa."
+}
+
+print("Chatbot Layanan Informasi Mahasiswa")
+print("Ketik 'exit' untuk keluar")
+
+while True:
+    user = input("Anda: ").lower()
+
+    if user == "exit":
+        print("Terima kasih telah menggunakan chatbot!")
+        break
+
+    if user in data:
+        print("Bot:", data[user])
+    else:
+        print("Bot: Maaf, saya belum paham pertanyaan Anda.")
