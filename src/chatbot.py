dd2d432a8b9debfe19942dcc626f552a7f2a4abd
keyshasalsabila <salsaazhr8@gmail.com>
2025-12-15 20:17:55 +0700

    Update chatbot.py

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,10 +1,12 @@
 # Data pertanyaan dan jawaban (kamus)
 data = {
-    "kapan jadwal kuliah": "Jadwal kuliah dapat dilihat di portal akademik.",
-    "bagaimana cara bayar ukt": "Pembayaran UKT melalui bank yang ditentukan kampus.",
-    "kapan pengisian KRS dilakukan": "Dilakukan setiap awal semester sesuai kalender akademik.",
-    "apakah kampus menyediakan beasiswa": "Ya, kampus menyediakan berbagai jenis beasiswa berdasarkan prestasi dan ekonomi.",
-    "dimana saya bisa lihat nilai akhir": "Nilai dapat dilihat melalui portal akademik mahasiswa."
+    "Kapan jadwal kuliah": "Jadwal kuliah dapat dilihat di portal akademik.",
+    "Bagaimana cara bayar ukt": "Pembayaran UKT melalui bank yang ditentukan kampus.",
+    "Kapan pengisian KRS dilakukan": "Dilakukan setiap awal semester sesuai kalender akademik.",
+    "Apakah kampus menyediakan beasiswa": "Ya, kampus menyediakan berbagai jenis beasiswa berdasarkan prestasi dan ekonomi.",
+    "Dimana saya bisa lihat nilai akhir": "Nilai dapat dilihat melalui portal akademik mahasiswa.",
+    "Bagaimana cara cetak KRS": "Cetak KRS bisa melalui SIAKAD setelah KRS disetujui dosen wali.",
+    "Bagaimana cara mengajukan cuti kuliah": "Pengajuan cuti kuliah dilakukan dengan mengisi formulir cuti dan menyerahkannya ke bagian akademik."
 }
 
 print("Chatbot Layanan Informasi Mahasiswa")
